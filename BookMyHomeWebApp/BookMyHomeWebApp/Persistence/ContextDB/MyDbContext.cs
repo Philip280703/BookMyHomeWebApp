04e19f3375df3b357fc3045f36f05f8218db7ebd
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-02-27 12:47:45 +0100

    added dbcontext

---
+++ BookMyHomeWebApp/BookMyHomeWebApp/Persistence/ContextDB/MyDbContext.cs
@@ -0,0 +1,23 @@
+﻿using BookMyHome.Shared.Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Configuration;
+
+namespace BookMyHomeWebApp.Persistence.ContextDB
+{
+	public class MyDbContext : DbContext
+	{
+		protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+		{
+			
+		}
+
+
+		public DbSet<Bolig> BoligEF { get; set; }
+		public DbSet<Bruger> BrugerEF { get; set; }
+		public DbSet<Adresse> AresseEF { get; set; }
+		public DbSet<Anmeldelse> AnmeldelseEF { get; set; }
+		public DbSet<Booking> BookingEF { get; set; }
+		public DbSet<PostnummerOgBy> postnummerOgByEF { get; set; }
+
+	}
+}
