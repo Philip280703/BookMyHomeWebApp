dc28fb0df49001e790ffa6f993bb4a175db9b3ad
Mark Koch Ruge <mkru61677@edu.ucl.dk>
2025-03-04 15:31:00 +0100

    Booking repository

---
+++ BookMyHomeWebApp/BookMyHomeWebApp/Persistence/Repositories/BookingRepositorySql.cs
@@ -0,0 +1,43 @@
+﻿using BookMyHome.Shared.Domain.Models;
+using BookMyHomeWebApp.Persistence.ContextDB;
+
+namespace BookMyHomeWebApp.Persistence.Repositories
+{
+	public class BookingRepositorySql : IBookingRepository
+	{
+        MyDbContext db = new MyDbContext();
+        public BookingRepositorySql()
+        {
+            
+        }
+
+        public void AddBooking(Booking booking)
+		{
+			db.BookingEF.Add(booking);
+			db.SaveChanges();
+            Console.WriteLine("Booking added to database");
+        }
+
+        public bool DeleteBooking(int id)
+        {
+            var booking = db.BookingEF.Find(id);
+            if(booking == null)
+            {
+                return false;    
+            }
+            db.BookingEF.Remove(booking);
+            db.SaveChanges();
+            return true;
+        }
+
+        public List<Booking> GetAllBookings()
+		{
+			return db.BookingEF.ToList();
+		}
+
+        public Booking GetBooking(int id)
+        {
+            return db.BookingEF.Find(id);
+        }
+    }
+}
---
+++ BookMyHomeWebApp/BookMyHomeWebApp/Persistence/Repositories/IBookingRepository.cs
@@ -0,0 +1,11 @@
+﻿using BookMyHome.Shared.Domain.Models;
+namespace BookMyHomeWebApp.Persistence.Repositories
+{
+	public interface IBookingRepository
+	{
+		List<Booking> GetAllBookings();
+		Booking GetBooking(int id);
+		void AddBooking(Booking booking);
+		bool DeleteBooking(int id);
+	}
+}
