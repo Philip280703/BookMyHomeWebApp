f973a3f1d9fc6698753b6cdd87feccf57b195c8c
Philip Holm Møller <phmo61881@edu.ucl.dk>
2025-03-06 11:14:26 +0100

    gn

---
+++ BookMyHomeWebApp/BookMyHomeWebApp/Persistence/ContextDB/ConnString.cs
@@ -0,0 +1,10 @@
+﻿namespace BookMyHomeWebApp.Persistence.ContextDB
+{
+	public static class ConnString
+	{
+		public static string GetConnString()
+		{
+			return "indsæt conn string her";
+		}
+	}
+}
